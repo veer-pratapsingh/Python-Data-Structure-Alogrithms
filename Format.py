3ec702f73f14936236a551111a89a9e2df3e71e3
veer-pratapsingh <chaudharyreepa@gmail.com>
2023-01-29 00:08:02 +0530

    Day-1

---
+++ Format.py
@@ -0,0 +1,7 @@
+a = int(input("Enter the value of A:"))
+b = int(input("Enter the value of B:"))
+add = a+b
+sub = a-b
+mul = a*b
+div = a/b
+print("Addition = {0}, Subtract = {1}, Multiplication = {2}, Divison = {3}".format(add,sub,mul,div))
