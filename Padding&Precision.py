a76f70bddae7029d649dbe13eba0686a4a84b49a
Veer Pratap Singh <104273381+veer-pratapsingh@users.noreply.github.com>
2023-01-28 18:55:48 +0000

    Day-1

---
+++ Padding&Precision.py
@@ -0,0 +1,7 @@
+a = int(input("Enter the value of A:"))
+b = int(input("Enter the value of B:"))
+add = a+b
+sub = a-b
+mul = a*b
+div = a/b
+print("sum = %d, sub = %d, mul = %d, div = %15.3f"%(add,sub,mul,div))#using the PADDING and PRECISION
