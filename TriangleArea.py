2936b2e8a315077d094fa62fd2d29f0795e22f71
Veer Pratap Singh <104273381+veer-pratapsingh@users.noreply.github.com>
2023-01-30 02:26:44 +0530

    Day-3

---
+++ TriangleArea.py
@@ -0,0 +1,9 @@
+def area_triangle(base,height):
+    return base*height/2
+
+a_triangle1 = area_triangle(12,2)
+a_triangle2 = area_triangle(2,4)
+sum = a_triangle1 + a_triangle2
+print(sum)
+print(a_triangle1)
+print(a_triangle2)
