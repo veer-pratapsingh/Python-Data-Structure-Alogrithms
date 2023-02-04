bffd21800f939faefc735d2a7164c8939b2d7d48
Veer Pratap Singh <104273381+veer-pratapsingh@users.noreply.github.com>
2023-02-04 18:56:16 +0530

    Day-6

---
+++ string1.py
@@ -0,0 +1,34 @@
+name = "Vansh"
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[0:2])
+print(name[0:3])
+print(name[0:4])
+print(name[0:5])
+print(name[0:6])
+
+name1 = "Ipshita"
+print(name1[-1])
+print(name1[-2])
+print(name1[-3])
+print(name1[-4])
+print(name1[-5])
+print(name1[-6])
+print(name1[-7])
+
+fruti = "pineapple"
+print(fruti[1:4])
+print(fruti[1:5])
+print(fruti[1:6])
+print(fruti[1:7])
+print(fruti[1:8])
+print(fruti[1:-2])
+print(fruti[1:-3])
+print(fruti[1:-4])
+
+color = "purple"
+print(color[:4])
+print(color[4:])
