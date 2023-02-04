dfffc960d7416b95802c2e5337e4155d4cc102d7
Veer Pratap Singh <104273381+veer-pratapsingh@users.noreply.github.com>
2023-02-04 21:09:58 +0530

    Day-7

---
+++ stringindex.py
@@ -0,0 +1,25 @@
+name = "Raja & Rani"
+name.index("&")
+print(name.index("&"))
+print(name.index("R"))
+print(name.index("a"))
+print(name.index("j"))
+print(name.index("R", 1))
+print(name.index("a", 1))
+print(name.index("j", 1))
+print(name.index("R", 2))
+print(name.index("a", 2))   
+name1="Mountain"
+name1.upper()
+print(name1.upper())
+print(name1.lower())
+
+color = "    Green       "
+print(color.strip())
+print(color.count("e"))
+print(color.endswith(" "))
+
+num = "12345"
+print(num.isdigit())
+print(num.isalpha())
+print(num.isnumeric())
